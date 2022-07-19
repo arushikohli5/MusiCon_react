4cf4f8d9988e47e973e69fd6a063a7ab67da9a9c
Arushi Kohli <arushi.kohli@Geminisolutions.com>
2022-07-19 17:07:32 +0530

    added link to

--- src/App.js
+++ src/App.js
@@ -4,23 +4,33 @@ import Navbar from './components/Navbar';
 import Home from './components/home'
 import Category from './components/categories'
 import AboutUs from './components/aboutUs'
-import Organisers from './components/organisers' 
+import Organisers from './components/organisers'
 import ContactUs from './components/contactUs'
 import Footer from './components/footer'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route, Link, } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/contact" element={<ContactUs/>} >
+          </Route>
+          <Route path="/" exact element={<Navbar/>}>
+          </Route>
+        </Routes>
+      </Router>
+      {/* <Navbar/>
       <Home/>
       <Category/>
       <AboutUs/>
       <Organisers/>
       <ContactUs/>
-      <Footer/>
-    </div>
+      <Footer/> */}
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,51 +3,66 @@ import "./style.css";
 import img from "./imges/4logo.png";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import $ from "jquery";
+import { BrowserRouter as Router, Routes, Route, Link, Redirect, Navigate,useNavigate} from 'react-router-dom';
+import Home from '../components/home'
+import Category from '../components/categories'
+import AboutUs from '../components/aboutUs'
+import Organisers from '../components/organisers'
+import ContactUs from '../components/contactUs'
+import Footer from '../components/footer'
 
 
-const NavbarComp=()=>{
-    $(window).scroll(function(){
+const NavbarComp = () => {
+    $(window).scroll(function () {
         "use strict"
-        var top=$(window).scrollTop();
-        if(top>=80){
+        var top = $(window).scrollTop();
+        if (top >= 80) {
             $(".nava").addClass("secondary");
         }
         else
-            if($(".nava").hasClass("secondary")){
+            if ($(".nava").hasClass("secondary")) {
                 $(".nava").removeClass("secondary");
             }
     });
-    return(
-            <div>
+    return (
+        <div>
+        <div>
             <nav className="navbar navbar-fixed-top navbar-inverse navbar-expand-lg nava">
-            <div className="container">
-                <div className="row">
-                {/*<!--for putting the logo--> */}
-                <div className="navbar-header">
-                    <a href="." className="navbar-brand"><img src={img}/></a> 
-                    {/*<!--button for collapse--> */}
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                        <i class="fa-solid fa-dash"></i>
-                    </button>
-                </div>
+                <div className="container">
+                    <div className="row">
+                        {/*<!--for putting the logo--> */}
+                        <div className="navbar-header">
+                            <a href="." className="navbar-brand"><img src={img} /></a>
+                            {/*<!--button for collapse--> */}
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                                <i className="fa-solid fa-dash"></i>
+                            </button>
+                        </div>
 
-                {/* <!--for navbar collapsing--> */}
-                <div className="nava collapse navbar-collapse justify-content-end" id="navbarTogglerDemo01">
-                    {/* <!--navigation menus--> */}
-                    <ul className="nav navbar-nav navbar-right justify-content-end navi">
-                        <li><a href="">Home</a></li>
-                        <li><a href="">About Us</a></li>
-                        <li><a href="">Shedule</a></li>
-                        <li><a href="">Contact US</a></li>   
-                        <li><a href="" target="_blank"><span className="login">Login</span></a></li>
-                        <li><a href="./sign_up.html" target="_blank"><span className=" login">Sign Up</span></a></li>
-                    </ul>
-                </div>
+                        {/* <!--for navbar collapsing--> */}
+                        <div className="nava collapse navbar-collapse justify-content-end" id="navbarTogglerDemo01">
+                            {/* <!--navigation menus--> */}
+                            <ul className="nav navbar-nav navbar-right justify-content-end navi">
+                                <li><a href="">Home</a></li>
+                                <li><a href="">About Us</a></li>
+                                <li><a href="">Shedule</a></li>
+                                <li><a href=""><Link to="/contact">Contact US</Link></a></li>
+                                <li><a href="" target="_blank"><span className="login">Login</span></a></li>
+                                <li><a href="./sign_up.html" target="_blank"><span className=" login">Sign Up</span></a></li>
+                            </ul>
+                        </div>
 
-            </div>
-            </div>
-        </nav>
-            
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+        <Home/>
+        <AboutUs/>
+        <Category/>
+        <Organisers/>
+        <ContactUs/>
+        <Footer/>
         </div>
     )
 }
